5ef319cb9eb34d22d49fc25ccfa10a116f9c87fd
Vyacheslav Tretyakov <tretiakovslava@gmail.com>
2022-01-03 19:50:50 +0500

    Initial commit

---
+++ src/main/kotlin/com/weblabs/lab3server/Lab3ServerApplication.kt
@@ -0,0 +1,11 @@
+package com.weblabs.lab3server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab3ServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<Lab3ServerApplication>(*args)
+}
